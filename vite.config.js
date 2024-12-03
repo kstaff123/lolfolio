eff9b8975400a9e705c0beb41e4c1c1d94d2aa01
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2024-12-03 03:54:13 -0800

    fixed base link

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/lolfolio/",
+  base: "/",
   define: {
     'process.env': {}, // Ensure process.env is defined for legacy compatibility
   },
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_BASE_URL,
+        target: process.env.VITE_BASE_URL,
         changeOrigin: true,
       },
     },
