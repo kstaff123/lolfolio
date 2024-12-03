12adc51635189a23995fed94b40136d1b42f69da
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2024-12-03 02:35:22 -0800

    fixed backend link

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: proccess.env.VITE_API_BASE_URL,
+        target: process.env.VITE_API_BASE_URL,
         changeOrigin: true,
       },
     },
