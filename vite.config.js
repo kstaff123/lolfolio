9a9818caaf7fb5851a1f7b8d1397654a8df36ca0
kstaff123 <47585247+kstaff123@users.noreply.github.com>
2024-12-10 13:45:39 -0800

    serve changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/",
+  build: {
+    outDir: "dist", // Output directory for Vite build
+  },
   define: {
     'process.env': {}, // Ensure process.env is defined for legacy compatibility
   },
